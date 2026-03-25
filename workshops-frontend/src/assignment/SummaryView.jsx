79c7173b59bb1f8c2f057e2cca0341557d5b5dbd
JonasBernard <contact@jonas-bernard.dev>
2026-03-25 03:13:33 +0100

    Minor design fixes

--- workshops-frontend/src/assignment/SummaryView.jsx
+++ workshops-frontend/src/assignment/SummaryView.jsx
@@ -1,4 +1,4 @@
-import { Progress, Table } from "flowbite-react";
+import { Progress, Table, Accordion } from "flowbite-react";
 
 export default function SummaryView(props) {
     if (props.participants.length === 0 || props.workshops.length === 0) {
@@ -16,38 +16,48 @@ export default function SummaryView(props) {
     });
 
     return (
-    <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg md:rounded-bl-none">
-            <Table className="divide-y divide-gray-200 dark:divide-gray-700" hoverable={true}>
-                <thead>
-                    <tr>
-                        <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Workshop</td>
-                        {props.settings.numberOfWishesPerParticipant > 0 && Array.from({ length: props.settings.numberOfWishesPerParticipant }, (_, i) => (
-                            <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Anzahl {i + 1}. Wünsche</td>
-                        ))}
-                        <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Kapazität</td>
-                        <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Auslastung</td>
-                    </tr>
-                </thead>
-                <Table.Body>
-                    {props.workshops.map(workshop => (
-                        <Table.Row key={workshop.id}>
-                            <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap text-gray-900 dark:text-white">
-                                {workshop.name}
-                            </Table.Cell>
-                            {props.settings.numberOfWishesPerParticipant > 0 && Array.from({ length: props.settings.numberOfWishesPerParticipant }, (_, i) => (
-                                <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap" key={i}>{props.participants.filter(participant => participant.wishes[i] === workshop.name).length}</Table.Cell>
-                            ))}
-                            <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap">
-                                {workshop.capacity}
-                            </Table.Cell>
-                            <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap">
-                                <Progress size="md" color="indigo"
-                                    progress={100 * (countWishesPerWorkshop[workshop.name] || 0) / (workshop.capacity * props.settings.numberOfWishesPerParticipant)} />
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table>
-    </div>
+        <Accordion>
+            <Accordion.Panel isOpen={false}>
+                <Accordion.Title>Zusammenfassung</Accordion.Title>
+                <Accordion.Content className="dark:bg-slate-800">
+                
+
+                    <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg md:rounded-bl-none">
+                        <Table className="divide-y divide-gray-200 dark:divide-gray-700" hoverable={true}>
+                            <thead>
+                                <tr>
+                                    <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Workshop</td>
+                                    {props.settings.numberOfWishesPerParticipant > 0 && Array.from({ length: props.settings.numberOfWishesPerParticipant }, (_, i) => (
+                                        <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Anzahl {i + 1}. Wünsche</td>
+                                    ))}
+                                    <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Kapazität</td>
+                                    <td className="py-3.5 px-4 text-sm font-normal text-center text-gray-500 dark:text-gray-400">Auslastung</td>
+                                </tr>
+                            </thead>
+                            <Table.Body>
+                                {props.workshops.map(workshop => (
+                                    <Table.Row key={workshop.id}>
+                                        <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap text-gray-900 dark:text-white">
+                                            {workshop.name}
+                                        </Table.Cell>
+                                        {props.settings.numberOfWishesPerParticipant > 0 && Array.from({ length: props.settings.numberOfWishesPerParticipant }, (_, i) => (
+                                            <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap" key={i}>{props.participants.filter(participant => participant.wishes[i] === workshop.name).length}</Table.Cell>
+                                        ))}
+                                        <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap">
+                                            {workshop.capacity}
+                                        </Table.Cell>
+                                        <Table.Cell className="px-2 py-2 text-center text-sm whitespace-nowrap">
+                                            <Progress size="md" color="indigo"
+                                                progress={100 * (countWishesPerWorkshop[workshop.name] || 0) / (workshop.capacity * props.settings.numberOfWishesPerParticipant)} />
+                                        </Table.Cell>
+                                    </Table.Row>
+                                ))}
+                            </Table.Body>
+                        </Table>
+                </div>
+
+                </Accordion.Content>
+            </Accordion.Panel>
+        </Accordion>
     );
 }
--- workshops-frontend/src/components/Card.jsx
+++ workshops-frontend/src/components/Card.jsx
@@ -3,7 +3,7 @@ export default function Card(props) {
         className={
             props.extraStyle + " "
             + (props.bgColor || "bg-white dark:bg-gray-800 ")
-            + "p-4 m-2 overflow-hidden rounded-lg shadow-lg border-neutral-100 dark:border-none border-2 dark:bg-gray-800bg-blue-600"
+            + "p-4 m-2 overflow-hidden rounded-lg shadow-lg border-neutral-100 dark:border-none border-2 dark:bg-gray-800 bg-blue-600"
         }
     >
         {props.children}
